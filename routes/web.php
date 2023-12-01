0ece7f541e209f5f8163bc7144d33f8f7e56eef4
Anton Semenov <ansemennov@gmail.com>
2023-12-01 23:02:48 +0500

    Creating routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\TestController;
-use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,11 +20,11 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome');
 Route::get('test', TestController::class);
 
-Route::get('posts',[PostController::class, 'index'])->name('posts');
-Route::get('posts/create',[PostController::class, 'create'])->name('posts.create');
-Route::post('posts',[PostController::class, 'store'])->name('posts.store');
-Route::post('posts/{post}',[PostController::class, 'show'])->name('posts.show');
-Route::post('posts/{post}/edit',[PostController::class, 'edit'])->name('posts.edit');
-Route::put('posts/{post}',[PostController::class, 'update'])->name('posts.update');
-Route::delete('posts/{post}',[PostController::class, 'delete'])->name('posts.delete');
-Route::put('posts/{post}/like',[PostController::class, 'like'])->name('posts.like');
+Route::get('register',[RegisterController::class,'index'])->name('register');
+Route::post('register',[RegisterController::class,'store'])->name('register.store');
+
+Route::get('login',[LoginController::class,'index'])->name('login');
+Route::post('login',[LoginController::class,'store'])->name('login.store');
+
+
+
