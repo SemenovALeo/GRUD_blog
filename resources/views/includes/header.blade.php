cf220199fd8bd2a14a2f92504575ec79f8116979
Anton Semenov <ansemennov@gmail.com>
2024-01-07 22:03:21 +0500

    Creating views. Layout and website components

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <nav class="navbar navbar-expand-md bg-body-tertiary">
     <div class="container">
 
@@ -41,41 +40,3 @@
 
     </div>
 </nav>
-=======
-<header class="py-3 border-bottom">
-    <div class="container">
-        <div class="d-flex justify-content-between">
-            <div>
-                <ul class="list-unstyled d-flex">
-                    <li class="me-3">
-                        <a href="{{route('home')}}">
-                            Главная
-                        </a>
-                    </li>
-                    <li class="me-3">
-                        <a href="{{route('blog')}}">
-                            Статьи
-                        </a>
-                    </li>
-
-                </ul>
-            </div>
-            <div>
-
-                <ul class="list-unstyled d-flex">
-                    <li class="ms-3">
-                        <a href="{{route('register')}}">
-                            Регистрация
-                        </a>
-                    </li>
-                    <li class="ms-3">
-                        <a href="{{route('login')}}">
-                            Вход
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</header>
->>>>>>> origin/master
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,43 +5,25 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/css/bootstrap.min.css">
-<<<<<<< HEAD
-    @stack('css')
     <title>@yield('page.title', config('app.name'))</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/css/bootstrap.min.css">
+    @stack('css')
     <style>
         .container{max-width: 720px}
         .required:after{ content: '*'; color: red; }
     </style>
 </head>
 <body>
-
-<div class="d-flex flex-column justify-content-between min-vh-100 ">
-=======
-    <title>@yield('page.title', config('app.name'))</title>
-</head>
-<body>
-
-<div class="d-flex flex-column justify-content-between min-vh-100 text-center">
->>>>>>> origin/master
-
-    @include('includes.header')
-
-    <main class="flex-grow-1 py-3">
-        @yield('content')
-    </main>
-
-    @include('includes.footer')
-</div>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/js/bootstrap.min.js">
-<<<<<<< HEAD
-</script>
-@stack('js')
-=======
-
-</script>
->>>>>>> origin/master
+    <div class="d-flex flex-column justify-content-between min-vh-100">
+        @include('includes.header')
+        <main class="flex-grow-1 py-3">
+            @yield('content')
+        </main>
+        @include('includes.footer')
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/js/bootstrap.min.js"></script>
+    @stack('js')
 </body>
 </html>
 
