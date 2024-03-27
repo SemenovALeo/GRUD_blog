3be71f4b58d764088d362d7f11ddf7139a538b11
Anton Semenov <ansemennov@gmail.com>
2024-03-27 21:29:38 +0500

    implemented input/output from the database. Registering users, adding posts.

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,40 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
     public function index(Request $request)
     {
-        $search = $request->input('search');
-        $category_id = $request->input('category_id');
+        $validated = $request->validate([
+            'search' => ['nullable', 'string', 'max:50'],
+            'from_date' => ['nullable', 'string', 'date'],
+            'to_date' => ['nullable', 'string', 'date', 'after:from_date'],
+            'tag' => ['nullable', 'string', 'max:10'],
+        ]);
+
+        $query = Post::query()
+            ->where('published', true)
+            ->whereNotNull('published_at');
+
+        if ($search = $validated['search'] ?? null) {
+            $query->where('title', 'like', "%{$search}%");
+        }
+
+        if ($fromDate = $validated['from_date'] ?? null) {
+            $query->where('published_at', '>=', new Carbon($fromDate));
+        }
+
+        if ($toDate = $validated['to_date'] ?? null) {
+            $query->where('published_at', '<=', new Carbon($toDate));
+        }
+
+        if ($tag = $validated['tag'] ?? null) {
+            $query->whereJsonContains('tags', $tag);
+        }
 
 
-        $post = (object)[
-            'id' => 123,
-            'title' => 'Lorem ipsum dolor sit amet.',
-            'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, quidem.',
-            'category_id' => 1,
-        ];
+        $posts = $query->latest('published_at')
+            ->paginate(12);
 
-        $posts = array_fill(0,10,$post);
-
-        $posts = array_filter($posts, function ($post) use ($search,$category_id){
-            if ($search && !str_contains(strtolower($post->title),strtolower($search))){
-                return false;
-            }
-            if ($category_id && $post->category_id != $category_id){
-                return false;
-            }
-            return  true;
-        });
-
-        $categories = [null=>__('Все категории'),
-            1 => __('Первая категория'), 2 => __('Вторая категория')];
-
-
-        return view('blog.index',compact('posts','categories'));
+        return view('blog.index', compact('posts'));
     }
 
     public function show($post)
